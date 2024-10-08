b559941b768af8d5a880de0162b7da8e1737db33
samreensaba <samren_saba@yahoo.com>
2024-10-08 14:53:59 +0100

    final commit

--- main.py
+++ main.py
@@ -1,117 +1,134 @@
-import os, time
+import os,time
 
-title = "\033[34mTODO LIST MANAGEMENT SYSTEM\033[0m"
+os.system("clear")
 
-print(f"{title: ^60}")
-print()
+title = "\033[34mTODO List Management System\033[0m"
 toDoList = []
 
 
 
-def prettyprint():
+def add():
+  time.sleep(2)
+  os.system("clear")
+  print("\033[36mADD\033[0m")
   print()
-  print("Here is your todo list")
-  for row in toDoList:
-    for item in row:
-      print(f"{item: ^10}", end=" | ")
-    print()
   print()
+  name = input("WHat do you want to add? ")
+  dueDate = input("What is the due date? ")
+  priority = input("What is the priority(high/medium/low)? ")
+  newList = [name.capitalize(), dueDate, priority.capitalize()]
+  toDoList.append(newList)
 
 def printPriority(userPriority):
-  print()
-  priorityList = []
+    print()
+    priorityList = []
   
-  for row in toDoList:
-    if userPriority in row:
-      priorityList.append(row)
-      
-  for list in priorityList:
-    for item in list:
-      print(f"{item: ^10}", end=" | ")
+    for row in toDoList:
+      if userPriority in row:
+        priorityList.append(row)
+        
+    for list in priorityList:
+      for item in list:
+        print(f"{item: ^10}", end=" | ")
+      print()
     print()
+
+def view():
+  print("\033[38mVIEW\033[0m")
   print()
-
-
-
-
-while True:
-  print("""  1: ADD
-  2: VIEW
-  3: EDIT
-  4: REMOVE""")
-  menu = input("> ")
-  if menu.lower() == "add" or menu == "1":
-    time.sleep(1)
-    os.system("clear")
-    print("ADD")
+  print()
+  viewOption = input("1: View All\n2: View Priority\n> ")
+  if viewOption == "1":
     print()
+    print("Here is your TODO List")
     print()
-    name = input("What to add?: ")
-    dueDate = input("When is it due?: ")
-    priority = input("Priority(High/Medium/Low)?: ")
-
-    newList = [name, dueDate, priority.lower()]
-    toDoList.append(newList)
-    
-    print()
-    print()
-    print("Added Sucessfully")
-  elif menu.lower() == "remove" or menu == "4":
-    name = input("Wwhat do you want to remove from the to do list?: ")
-    
     for row in toDoList:
-      if name in row:
-        confirm = input(f"Are you sure you want to remove {name} from the list?y/n: ")
-        if confirm == "y":
-          toDoList.remove(row)
-          print(f"{name} removed from the list.")
-        else:
-          print(f"{name} not removed.")
-      else:
-        print(f"{name} was not in the list")
-  elif menu.lower() == "view" or menu == "2":
-    print("VIEW")
+      for item in row:
+        print(f"{item: ^10}", end=" | ")
+      print()
     print()
-    print()
-    print("""1: View All
-          2: View Priority""")
-    view = input("> ")
-    if view == "2":
-      askPriority = input("Which priority?(High/Medium/Low): ")
-      if askPriority.lower() == "high":
-        print("High Priority List")
-        printPriority("high")
-      elif askPriority.lower() == "medium":
-        print("medium Priority List")
-        printPriority("medium")
-      elif askPriority.lower() == "low":
-        print("Low Priority List")
-        printPriority("low")
-      else:
-        print("Invalid Response")  
-      
+  elif viewOption == "2":
+    userPriority = input("1: High\n2: Medium\n3: Low\n> ")
+    if userPriority == "1":
+      userPriority = "High"
+      print("High priority List:")
+      print()
+      printPriority(userPriority)
+    elif userPriority == "2":
+      userPriority = "Medium"
+      print("Mediun priority List:")
+      print()
+      printPriority(userPriority)
+    elif userPriority == "3":
+      userPriority = "Low"
+      print("Low priority List:")
+      print()
+      printPriority(userPriority)
     else:
-      prettyprint()
-      
-    #time.sleep(5)
-    #os.system("clear")
-    
-  elif menu.lower() == "edit" or menu == "3":
-    editItem = input("Which item do you want to edit? ")
-    for row in toDoList:
-      if editItem in row:
-        newItem = input("What is the new name?")
+      print("Invali response")
+  else:
+    print("Invalid response")
+
+def edit():
+  print("\033[37mEDIT\033[0m")
+  print()
+  print()
+  findList = input("Which item do you want to edit? ")
+  isavailable = False
+  for row in toDoList:
+      if findList.capitalize() in row:
+        isavailable = True
+  if isavailable:
+        newItem = input("What is the new name?: ")
         newDate = input("New Due Date?: ")
-        newPriority = input("What is the new priority? ")
+        newPriority = input("What is the new priority?: ")
         toDoList.remove(row)
-        newRow = [newItem, newDate, newPriority]
+        newRow = [newItem.capitalize(), newDate, newPriority.capitalize()]
         
         toDoList.append(newRow)
-      
+        print()
+        print("Changed Sucessfully")
+    
   else:
-    print("Invalid input, try again.")
+    print(f"\033[32m{findList}\033[0m not found in TODO List")
 
+def remove():
+  name = input("What do you want to delete? ")
+  isAvailable = False
+  for row in toDoList:
+    if name in row:
+      isAvailable = True
+  if isAvailable:
+    confirmation = input(f"Are you sure you want to delete {name}?y/n ")
+    if confirmation == "y":
+      toDoList.remove(row)
+      print(f"{name} deleted")
+    elif confirmation == "n":
+      print(f"{name} not deleted")
+    else:
+      print("Invalid response")
+  else:
+    print(f"{name} not found in list")
+
+
+
+
+
+print(f"{title: ^50}")
+
+while True:
+  menu = input("1: ADD\n2: VIEW\n3: EDIT\n4: REMOVE\n> ")
+  if menu == "1":
+    add()
+  elif menu == "2":
+    view()
+  elif menu == "3":
+    edit()
+  elif menu == "4":
+    remove()
+  else:
+    print("Invalid Option")
+  print()
   time.sleep(3)
   os.system("clear")
-  print(f"{title: ^60}")
-  
+  print(f"{title: ^50}")
